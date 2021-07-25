29bb20b5f453ecaf20c1ce4198be1a1590529d21
ChristianSama <christiansama1601@gmail.com>
2021-07-25 04:41:59 -0500

    create reference between tweets and users

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_25_075709) do
+ActiveRecord::Schema.define(version: 2021_07_25_092628) do
 
   create_table "tweets", force: :cascade do |t|
     t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_tweets_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -33,4 +35,5 @@ ActiveRecord::Schema.define(version: 2021_07_25_075709) do
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
+  add_foreign_key "tweets", "users"
 end
