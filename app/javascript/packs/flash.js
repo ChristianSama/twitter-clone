0a3f3905ee081a7b351e50098757b3891f223141
ChristianSama <christiansama1601@gmail.com>
2021-08-10 19:17:25 -0500

    minor fixes to flash and profile views

--- app/javascript/packs/flash.js
+++ app/javascript/packs/flash.js
@@ -1,3 +1,8 @@
-const notice = document.querySelector(".notice");
+let notice = document.querySelector(".notice");
+
+if (notice === null) {
+  notice = document.querySelector(".alert-notice");
+}
+
 notice.style.opacity = '0';
 notice.addEventListener('transitionend', () => notice.remove());
