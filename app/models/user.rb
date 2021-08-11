046ecb564bc1fc95bb29082af6c22b275b0e77c9
ChristianSama <christiansama1601@gmail.com>
2021-08-10 19:18:03 -0500

    add follow model and associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_one :profile, dependent: :destroy
-  has_many :tweets
+  has_many :tweets, dependent: :destroy
+
+  has_many :received_follows, foreign_key: :followed_user_id, class_name: "Follow"
+  has_many :followers, through: :received_follows, source: :follower
+
+  has_many :given_follows, foreign_key: :follower_id, class_name: "Follow"
+  has_many :followed_users, through: :given_follows, source: :followed_user
 
   after_create :init_profile
 
